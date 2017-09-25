63b438096f70ec613afbcac0cf2485e9b5717e38
David Guijarro <guijarro.dav@gmail.com>
2017-09-25 12:14:31 +0200

    Typedefs ready!
    Cosmetic change :)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+
+import schema from './schema';
 
 const app = express();
 
@@ -6,11 +10,26 @@ const rdb = require('rethinkdbdash')({
 	servers: [{ host: process.env.RDB_HOST, port: process.env.RDB_PORT }],
 });
 
+app.use(
+	'/graphql',
+	bodyParser.json(),
+	graphqlExpress({
+		schema: schema,
+	})
+);
+
+app.use(
+	'/graphiql',
+	graphiqlExpress({
+		endpointURL: '/graphql',
+	})
+);
+
 app.get('/', (req, res) => {
 	rdb.tableList().run();
 	res.send('Hello World!');
 });
 
 app.listen(process.env.PORT || 3000, () => {
-	console.log('Example app listening on port 3000!');
+	console.log(`DevCon server listening on port ${process.env.PORT || 3000}!`);
 });
