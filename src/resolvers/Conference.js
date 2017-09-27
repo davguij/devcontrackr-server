a710e28c428bce6251feb0a72103d84baa9771f4
David Guijarro <guijarro.dav@gmail.com>
2017-09-27 15:40:53 +0200

    Setup resolvers for Speakers

--- src/resolvers/Conference.js
+++ src/resolvers/Conference.js
@@ -3,13 +3,24 @@ import DBConnector from '../connectors/db';
 const db = new DBConnector();
 
 const ConferenceResolvers = {
-	Query: {
+	RootQuery: {
 		conferences: () => db.getAll('Conferences'),
 		conference: (_, { id }) => db.getOne('Conferences', id),
+		speakers: () => db.getAll('Speakers'),
+		speaker: (_, { id }) => db.getOne('Speakers', id),
 	},
-	Mutation: {
+	Conference: {
+		speakers({ speakers }, _, context) {
+			console.log(speakers);
+			// need a table join here...!
+			return [{ id: speakers[0].id, name: 'David Guijarro' }];
+		},
+	},
+	RootMutation: {
 		createConference: (_, { input }) => db.create('Conferences', input),
 		updateConference: (_, { input }) => db.update('Conferences', input),
+		createSpeaker: (_, { input }) => db.create('Speakers', input),
+		updateSpeaker: (_, { input }) => db.update('Speakers', input),
 	},
 };
 
